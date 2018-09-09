e0864842c8834eefa78f57e6478f6563395be0b0
Noe <VaescoNoe@gmail.com>
2018-09-08 23:24:03 -0500

    Procedures

--- src/cs/funciones/Test.java
+++ src/cs/funciones/Test.java
@@ -10,22 +10,45 @@ public class Test {
 		int empleadoId=100;
 		
 		try {
-			Connection con = Conexion.getConnection();
-			CallableStatement cs = null;
-			double salarioMensual;
-			
-			cs = con.prepareCall("{ ? = call get_employees_salary(?)}");
-			
-			cs.registerOutParameter(1,java.sql.Types.INTEGER);
-			cs.setInt(2, empleadoId);
-			cs.execute();
-			salarioMensual = cs.getDouble(1);
-			cs.close();
-			System.out.println("Empleado con el ID:"+empleadoId);
-			System.out.println("Salario $"+salarioMensual);
+			procedure(empleadoId);
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
+	
+	public static double function(int empleadoId) throws SQLException {
+		Connection con = Conexion.getConnection();
+		CallableStatement cs = null;
+		double salarioMensual;
+		
+		cs = con.prepareCall("{ ? = call get_employees_salary(?)}");
+		
+		cs.registerOutParameter(1,java.sql.Types.INTEGER);
+		cs.setInt(2, empleadoId);
+		cs.execute();
+		salarioMensual = cs.getDouble(1);
+		cs.close();
+		return salarioMensual;
+	}
+	
+	public static void procedure(int empleadoId) throws SQLException {
+		double incrementoSalario = 1.1;
+		Connection con;
+		con=Conexion.getConnection();
+		Statement stmt = null;
+		ResultSet rs = null;
+		CallableStatement cs = null;
+		
+		stmt = con.createStatement();
+			System.out.println("Aumento del 10% al empleado:"+empleadoId);
+		cs = con.prepareCall("{call set_employee_salary(?,?)}");
+		cs.setInt(1, empleadoId);
+		cs.setDouble(2,	incrementoSalario);
+		cs.execute();
+		cs.close();
+		
+		System.out.println("Nuevo salario:"+function(empleadoId));
+	}
+	
 }
